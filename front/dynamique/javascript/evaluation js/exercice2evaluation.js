8505fe33d599199e89851cb5b21eda48da07aeeb
tinathinhinene <tina.goblet@gmail.com>
2024-07-23 10:32:02 +0200

    marqueur

--- front/dynamique/javascript/evaluation js/exercice2evaluation.js
+++ front/dynamique/javascript/evaluation js/exercice2evaluation.js
@@ -1,1 +1,10 @@
+let i;
 let n;
+function table_multiplication(n){
+for(i=1; i<=10; i++){
+let resultat=i*n;
+
+document.write(n+ "x" +i+ "="+n *i+"<br>");
+}}
+n=prompt("entrer un nombre");
+table_multiplication(n);
---
+++ front/dynamique/javascript/evaluation js/exercice3jsevaluation.js
@@ -0,0 +1,13 @@
+     let prenom = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+{
+
+ 
+    let x=prompt("veuillez choisir un prénom");
+    if(x==="audrey"||"aurelien"||"flavien"||"jeremy"||"laurent"||"melik"||"nouara"||"salem"||"samuel"||"stephane"){
+       MyArray.pop(prenom);
+        prenom.push();
+        
+    }
+        console.log(prenom);
+}
+
